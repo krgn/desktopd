ae1ed1045de94787878ec8f5422cc69da32ad121
Karsten Gebbert <k@ioctl.it>
2021-04-29 14:23:40 +0200

    support focussing tabs in http api

--- native/src/http.rs
+++ native/src/http.rs
@@ -1,6 +1,7 @@
 use crate::error::DesktopdError;
 use crate::message::{CliRequest, DesktopdMessage};
 use crate::state::{GlobalState, Tx};
+use log::error;
 use log::info;
 use std::env;
 use tide::Request;
@@ -14,9 +15,23 @@ pub async fn get_clients(req: Request<(GlobalState, Tx)>) -> tide::Result {
 
 pub async fn post_command(mut req: Request<(GlobalState, Tx)>) -> tide::Result {
     let msg: CliRequest = req.body_json().await?;
-    let (_state, tx) = req.state();
-    tx.unbounded_send(DesktopdMessage::CliRequest(msg))
-        .expect("Sending message failed");
+    let (state, tx) = req.state();
+
+    use CliRequest::*;
+    match msg {
+        FocusWindow { .. } => tx
+            .unbounded_send(DesktopdMessage::CliRequest(msg.clone()))
+            .expect("Sending message failed"),
+        FocusTab { .. } => {
+            for (peer_addr, peer) in state.lock().unwrap().get_browser_connections() {
+                match peer.unbounded_send(DesktopdMessage::CliRequest(msg.clone())) {
+                    Ok(_) => info!("Successfully sent focus-tab message to browsers"),
+                    Err(e) => error!("Could not send message to browser {}: {}", peer_addr, e),
+                }
+            }
+        }
+    }
+
     Ok("Ok".into())
 }
 
